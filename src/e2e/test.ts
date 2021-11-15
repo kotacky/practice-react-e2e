d942bc9dae32b143ed56bc301bae08a12bcee7b2
kotacky <wiss1.ide@gmail.com>
2021-11-15 15:53:46 +0900

    新規作成

---
+++ src/e2e/test.ts
@@ -0,0 +1,13 @@
+import 'testcafe';
+import { Selector } from 'testcafe';
+
+// 1. テストする画面
+fixture('Getting started').page('http://devexpress.github.io/testcafe/example');
+
+test('名前の入力確認テスト', async (t: TestController) => {
+  await t
+    .typeText('#developer-name', 'ほげ山ふが太郎') // 2. 名前の入力
+    .click('#submit-button') // 3. Submitボタンのクリック
+    .expect(Selector('#article-header').innerText) // 4-a. 表示項目の指定
+    .eql('Thank you, ほげ山ふが太郎!'); // 4-b. 表示の確認
+});
